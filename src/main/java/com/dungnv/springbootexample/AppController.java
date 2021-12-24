88760317d70c38cddb86494c38bc43e4d832749f
nguyendung230699 <dung.230799@gmail.com>
2021-12-24 10:17:07 +0700

    update function edit,delete,create

--- src/main/java/com/dungnv/springbootexample/AppController.java
+++ src/main/java/com/dungnv/springbootexample/AppController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -17,4 +21,35 @@ public class AppController {
 		model.addAttribute("listProducts", listProducts);
 		return "index";
 	}
+	@RequestMapping("/new")
+	public String showNewProductForm(Model model) {
+		Product product = new Product();
+		model.addAttribute("product", product);
+		
+		return "new_product";
+		
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+	    service.save(product);
+	     
+	    return "redirect:/";
+	}
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+	    ModelAndView mav = new ModelAndView("edit_product");
+	    Product product = service.get(id);
+	    mav.addObject("product", product);
+	     
+	    return mav;
+	}
+	@RequestMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") int id) {
+	    service.delete(id);
+	    return "redirect:/";   
+	}
+
+	
+	
 }
